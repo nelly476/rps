6f2b1dfe882cc37b2f050698342cf1a620e976cd
nelly476 <nellybulatova12@gmail.com>
2022-08-07 11:04:06 +0300

    add ui

--- game.js
+++ game.js
@@ -1,4 +1,27 @@
-function computerPlay() {
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+const score = document.querySelector("#score");
+const computerWon = document.querySelector("#computerWon");
+const playerWon = document.querySelector("#playerWon");
+const finish = document.querySelector("#finish");
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach((button) =>
+  button.addEventListener("click", () => {
+    player = button.textContent.toLowerCase();
+    computer = computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+  })
+);
+
+function computerTurn() {
   let random = Math.random();
   if (random <= 0.3333) {
     return "paper";
@@ -9,59 +32,44 @@ function computerPlay() {
   }
 }
 
-function playRound() {
-  const computerSelection = computerPlay();
-  let playerSelection = prompt("Choose something", "rock");
-  if (
-    computerSelection === "rock" &&
-    playerSelection.toLowerCase() === "paper"
-  ) {
-    userScore++;
-    return win;
-  } else if (
-    computerSelection === "paper" &&
-    playerSelection.toLowerCase() === "rock"
-  ) {
-    computerScore++;
-    return lose;
-  } else if (
-    computerSelection === "paper" &&
-    playerSelection.toLowerCase() === "scissors"
-  ) {
-    userScore++;
-    return win;
-  } else if (
-    computerSelection === "scissors" &&
-    playerSelection.toLowerCase() === "paper"
-  ) {
-    computerScore++;
-    return lose;
-  } else if (
-    computerSelection === "scissors" &&
-    playerSelection.toLowerCase() === "rock"
-  ) {
-    userScore++;
-    return win;
-  } else if (
-    computerSelection === "rock" &&
-    playerSelection.toLowerCase() === "scissors"
-  ) {
-    computerScore++;
-    return lose;
-  } else {
-    return tie;
+let playerScore = 0;
+let computerScore = 0;
+
+function addPoint() {
+  playerScore++;
+  playerWon.textContent = `Your score: ${playerScore}`;
+  if (playerScore == 5) {
+    alert("You won the game!");
   }
 }
 
-let win = "You win";
-let lose = "You lose";
-let tie = "It's a tie";
+function noPoint() {
+  computerScore++;
+  computerWon.textContent = `Computer's score ${computerScore}`;
+  if (computerScore == 5) {
+    alert("You lost the game:(");
+  }
+}
 
-let userScore = parseInt(0);
-let computerScore = parseInt(0);
-
-for (let i = 0; i < 5; i++) {
-  console.log(playRound());
-  console.log("your score = " + userScore);
-  console.log("Computer's score = " + computerScore);
+function checkWinner() {
+  if (player == computer) return "It's a tie...";
+  else if (computer === "rock" && player === "paper") {
+    addPoint();
+    return "You win!";
+  } else if (computer === "paper" && player === "rock") {
+    noPoint();
+    return "You lose!";
+  } else if (computer === "paper" && player === "scissors") {
+    addPoint();
+    return "You win!";
+  } else if (computer === "scissors" && player === "paper") {
+    noPoint();
+    return "You lose!";
+  } else if (computer === "scissors" && player === "rock") {
+    addPoint();
+    return "You win!";
+  } else if (computer === "rock" && player === "scissors") {
+    noPoint();
+    return "You lose!";
+  }
 }
